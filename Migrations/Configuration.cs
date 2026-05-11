42e80bee3ce0cdd55050139c77ebb107c5bae29d
SUlav Shrestha <sulavss144@gmail.com>
2026-05-11 16:26:34 +1000

    added librarian, member dashboard and forced redirect to login if someone try to bypass url.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace LibrarySystem99.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LibrarySystem99.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LibrarySystem99.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
