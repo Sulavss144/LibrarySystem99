bcd2ab508f5b92bd0b0011dd00f772b47b58444b
SUlav Shrestha <sulavss144@gmail.com>
2026-05-18 01:41:54 +1000

    added book reviews features

---
+++ Migrations/202605171456095_AddBookReviews.cs
@@ -0,0 +1,39 @@
+﻿namespace LibrarySystem99.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBookReviews : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BookReviews",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        BookId = c.Int(nullable: false),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        Rating = c.Int(nullable: false),
+                        ReviewText = c.String(nullable: false, maxLength: 1000),
+                        CreatedDate = c.DateTime(nullable: false),
+                        UpdatedDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Books", t => t.BookId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.BookId)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BookReviews", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.BookReviews", "BookId", "dbo.Books");
+            DropIndex("dbo.BookReviews", new[] { "UserId" });
+            DropIndex("dbo.BookReviews", new[] { "BookId" });
+            DropTable("dbo.BookReviews");
+        }
+    }
+}
---
+++ Models/BookReview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibrarySystem99.Models
+{
+    public class BookReview
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5 stars.")]
+        [Display(Name = "Rating")]
+        public int Rating { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Your Review")]
+        public string ReviewText { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedDate { get; set; }
+
+        // Navigation properties
+        [ForeignKey("BookId")]
+        public virtual Book Book { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
